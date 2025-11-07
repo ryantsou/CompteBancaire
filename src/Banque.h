6f6050b5ec94b5429e3bbc14a0b57f56ba5bd8d1
Riantsoa Rajhonson <62847028+ryantsou@users.noreply.github.com>
2025-11-07 20:02:58 +0100

    Enhance Banque.h with client management features

--- src/Banque.h
+++ src/Banque.h
@@ -1,7 +1,9 @@
 #ifndef BANQUE_H
 #define BANQUE_H
 
-#include <vector>
+#include <vector>#include <unordered_map>
+#include <fstream>
+#include <sstream>
 #include "CompteBancaire.h"
 #include "Client.h"
 using namespace std;
@@ -15,7 +17,10 @@ public:
     CompteBancaire* trouverCompte(string numero);
     void afficherTousLesComptes() const;
     void creerCompte(Client client, string numero, double montantInitial);
-    void supprimerCompte(string numero);
+    void supprimerCompte(string numero);    std::unordered_map<std::string, Client> clients;
+    void sauverClients();
+    void chargerClients();
+    void creerClient(const Client& client);
     bool transfererArgent(string numeroSource, string numeroDestination, double montant);
 };
 
